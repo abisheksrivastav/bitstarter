dda878e5db73fdd1116106a350401870e33c5431
Ubuntu <ubuntu@ip-172-31-3-242.us-west-2.compute.internal>
2013-07-20 11:37:22 +0000

    Edited product desc in bitstarter.html

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-var htmlfile = "bitstarter.html";
+var htmlfile = "abi.html" ;
 
 var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
     var html = fs.readFileSync(htmlfile).toString();
-  response.send(html);
+    response.send(html);
 });
 
 var port = process.env.PORT || 8080;

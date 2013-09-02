e5665f863e35087573905a5bd8577c3f8cda2f24
abisheksrivastav <abisheksrivastav@gmail.com>
2013-09-02 10:48:57 +0000

    Added abi2.html, about.html and web.js.

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
-var htmlfile = "abi.html" ;
+var htmlfile = "abi2.html" ;
+var htmlfile2 = "about.html" ;
 
 var app = express.createServer(express.logger());
 
@@ -9,6 +10,17 @@ app.get('/', function(request, response) {
     var html = fs.readFileSync(htmlfile).toString();
     response.send(html);
 });
+app.get('/about', function(request, response) {
+    var html2 = fs.readFileSync(htmlfile2).toString();
+    response.send(html2);
+});
+
+app.get('/home', function(request, response) {
+    var html3 = fs.readFileSync(htmlfile).toString();
+    response.send(html3);
+});
+
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
